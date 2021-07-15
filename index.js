3636460b7b9e6ec4be5fdbd8163f7a65afcc4db3
Asif <aK788955@gmail.com>
2021-07-15 15:27:45 +0530

    adding script

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port =process.env.PORT || 3000;
+const port =process.env.PORT || 30000;
 const { mongoose } = require('./db.js');
 var employeeController = require('./controllers/employeeController.js');
 var router = express.Router();
 var path=require('path');
+var app=express();
 
-router.use(express.static(path.join(__dirname,'public')));
-router.get('*',(req,res)=>{
+
+
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'public')));
+app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'));
 })
 
-var app = express();
-app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:4200' }));
-
-app.listen(port, () => console.log('Server started at port : 3000'));
+app.listen(port, () => console.log(`Server started at port : ${port}`));
 
 app.use('/employees', employeeController);
