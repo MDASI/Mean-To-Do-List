070c8aee756a58697d59de8cd5ba18f9d3bf2d19
Asif <aK788955@gmail.com>
2021-07-15 13:54:22 +0530

    adding frontend

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 const port =process.env.PORT || 3000;
 const { mongoose } = require('./db.js');
 var employeeController = require('./controllers/employeeController.js');
+var router = express.Router();
+var path=require('path');
 
-
-
+router.use(express.static(path.join(__dirname,'public')));
+router.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
 
 var app = express();
 app.use(bodyParser.json());
