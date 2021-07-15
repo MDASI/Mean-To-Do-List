b06a6256870354f4f7b91d004a083a2b80d28a66
Asif <aK788955@gmail.com>
2021-07-15 11:57:04 +0530

    adding script3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var employeeController = require('./controllers/employeeController.js');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://gifted-agnesi-7f19ef.netlify.app' }));
+app.use(cors({ origin: 'http://localhost:4200' }));
 
 app.listen(port, () => console.log('Server started at port : 3000'));
 
