de23548cd7970c39ba633b5d0e53f0e8218bb35f
Asif <aK788955@gmail.com>
2021-07-15 17:40:20 +0530

    adding script

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/crud', (err) => {
+mongoose.connect("mongodb+srv://asif1234:1437869292@cluster0.rodhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err) => {
     if (!err)
         console.log('MongoDB connection succeeded.');
     else
