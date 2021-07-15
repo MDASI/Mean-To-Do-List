ea53b63892e54d88fe2fb21c8ff05554ec93c837
Asif <aK788955@gmail.com>
2021-07-15 15:28:28 +0530

    adding script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port =process.env.PORT || 30000;
+const port =process.env.PORT || 3000;
 const { mongoose } = require('./db.js');
 var employeeController = require('./controllers/employeeController.js');
 var router = express.Router();
